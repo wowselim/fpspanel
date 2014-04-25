87f4785ae39ff52bffb712e612071c9c2ff9acba
Selim Dincer <wowselim@live.de>
2014-04-25 14:44:33 +0200

    First version

---
+++ FPSPanel.java
@@ -0,0 +1,30 @@
+import java.awt.Graphics;
+import javax.swing.*;
+
+class FPSPanel extends JPanel implements Runnable {
+  private volatile int currentFPS;
+  private JLabel lblFPS;
+  
+  public FPSPanel() {
+    currentFPS = 0;
+    setOpaque(false);  
+    lblFPS = new JLabel();
+    add(lblFPS);
+    new Thread(this).start();
+  }
+  
+  public void run() {
+    while(true) {
+      try {
+        Thread.sleep(1000L);
+        lblFPS.setText(String.valueOf(currentFPS) + " FPS");
+        currentFPS = 0;
+      } catch (InterruptedException e) { }
+    }
+  }
+  
+  public void paintComponent(Graphics g) {
+    super.paintComponent(g);
+    currentFPS++;
+  }
+}
